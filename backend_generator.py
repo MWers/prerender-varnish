f33a1e644ce5001fb428962d83412fdc7134d429
Matt Walker <matt@suckerfish.net>
2013-11-10 20:55:49 -0500

    added options parsing to backend generator, fixed output issue

--- backend_generator.py
+++ backend_generator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #
 # TODO: Build out script using the following flow:
 # - Fingerprint existing prerender_backend.vcl file contents
@@ -12,32 +14,65 @@
 # - Notify on any errors
 #
 
+import optparse
 import socket
 
-# TODO: Check to see if these were passed as arguments
-domain = "prerender.herokuapp.com"
-port = 80
+desc = """Generate a Varnish backend configuration given a hostname."""
 
-TEMPLATE_NODE = '''
-{.backend = {.host = "%s";.port = "%s";}.weight = 1;}
-'''
-# TEMPLATE_NODE = '''    {
-#         .backend = {
-#             .host = "%s";
-#             .port = "80";
-#         }
-#         .weight = 1;
-#     }
+
+parser = optparse.OptionParser(description=desc)
+parser.add_option('--hostname',
+                  dest='hostname',
+                  help='prerender.io rendering backend hostname',
+                  default='prerender.herokuapp.com')
+parser.add_option('-p', '--port',
+                  dest='port',
+                  help='prerender.io rendering backend port',
+                  type='int',
+                  default=80)
+parser.add_option('-d', '--dest',
+                  dest='varnish_backend_conf',
+                  help='varnish backend configuration file to overwrite',
+                  default='/etc/varnish/prerender_backend.vcl')
+parser.add_option('--dry-run',
+                  action='store_true',
+                  dest='dry_run',
+                  help='output to stdout, don\'t write to conf file',
+                  default=False)
+(opts, args) = parser.parse_args()
+
+# TEMPLATE_NODE = '''
+# {.backend = {.host = "%s";.port = "%s";}.weight = 1;}
 # '''
+TEMPLATE_NODE = '''    {
+        .backend = {
+            .host = "%s";
+            .port = %d;
+        }
+        .weight = 1;
+    }
+'''
 
 TEMPLATE_DIRECTOR = '''director prerender random {
-%s
-}'''
+%s}'''
 
-(_, _, addrs) = socket.gethostbyname_ex(domain)
+(_, _, addrs) = socket.gethostbyname_ex(opts.hostname)
 
 # TODO: Check for duplicate addresses
 
 # FIXME: Determine why these strings are being improperly output
-nodes = ''.join([repr(TEMPLATE_NODE % (addr, port)) for addr in addrs])
+nodes = ''.join([(TEMPLATE_NODE % (addr, opts.port)) for addr in addrs])
 print TEMPLATE_DIRECTOR % nodes
+
+
+def main():
+    return
+    # generate_homepages()
+    # copy_to_working_dir()
+    # customize_for_servers()
+    # deploy_to_production()
+    # log_generation()
+
+
+if __name__ == '__main__':
+    main()
