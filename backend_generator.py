36d24fc1f05ce77e9213f823ca9e0e9978672dff
Matt Walker <matt@suckerfish.net>
2013-11-10 22:58:17 -0500

    Added code to backend_generator.py to compare generated director to existing configuration and overwrite configuration if they differ

--- backend_generator.py
+++ backend_generator.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 
 #
-# TODO: Build out script using the following flow:
-# - Fingerprint existing prerender_backend.vcl file contents
+# Script uses the following flow:
+# - Hash existing prerender_backend.vcl file contents
 # - Get ip addresses for rendering backend
 # - Make sure that there was at least one ip address returned
 # - Build backend node array
 # - Add backend node array to director
-# - Fingerprint generated director
-# - If fingerprints differ:
+# - Hash generated director
+# - If hashes differ:
 #     - Update prerender_backend.vcl
 #     - Reload Varnish configuration
 # - Notify on any errors
 #
+# TODO: Add logging
+# TODO: Add notifications
+# TODO: Add error checking
+# TODO: Add Varnish reloading
+#
 
+import hashlib
 import optparse
 import socket
 
 desc = """Generate a Varnish backend configuration given a hostname."""
-
 
 parser = optparse.OptionParser(description=desc)
 parser.add_option('--hostname',
@@ -31,7 +36,7 @@ parser.add_option('-p', '--port',
                   type='int',
                   default=80)
 parser.add_option('-d', '--dest',
-                  dest='varnish_backend_conf',
+                  dest='backend_conf',
                   help='varnish backend configuration file to overwrite',
                   default='/etc/varnish/prerender_backend.vcl')
 parser.add_option('--dry-run',
@@ -41,10 +46,7 @@ parser.add_option('--dry-run',
                   default=False)
 (opts, args) = parser.parse_args()
 
-# TEMPLATE_NODE = '''
-# {.backend = {.host = "%s";.port = "%s";}.weight = 1;}
-# '''
-TEMPLATE_NODE = '''    {
+TPL_NODE = '''    {
         .backend = {
             .host = "%s";
             .port = %d;
@@ -53,26 +55,55 @@ TEMPLATE_NODE = '''    {
     }
 '''
 
-TEMPLATE_DIRECTOR = '''director prerender random {
-%s}'''
+TPL_DIRECTOR = '''director prerender random {
+%s}
+'''
 
-(_, _, addrs) = socket.gethostbyname_ex(opts.hostname)
+# Get ip addresses for rendering backend
+(_, _, rawaddrs) = socket.gethostbyname_ex(opts.hostname)
+rawaddrs.sort()
+addrs = set(rawaddrs)
 
-# TODO: Check for duplicate addresses
+# Make sure that there was at least one ip address returned
+if not addrs:
+    1  # FIXME: Send notification and exit
 
-# FIXME: Determine why these strings are being improperly output
-nodes = ''.join([(TEMPLATE_NODE % (addr, opts.port)) for addr in addrs])
-print TEMPLATE_DIRECTOR % nodes
+# Build backend node array and director
+nodes = ''.join([(TPL_NODE % (addr, opts.port)) for addr in addrs])
+director = TPL_DIRECTOR % (nodes)
+
+# Compare generated director to existing director
+try:
+    filehash = hashlib.sha256(open(opts.backend_conf, 'rb').read()).hexdigest()
+except IOError:
+    filehash = ''
+stringhash = hashlib.sha256(director).hexdigest()
+
+if filehash != stringhash:
+    if not opts.dry_run:
+        backend_conf = open(opts.backend_conf, 'w')
+        backend_conf.write(director)
+        backend_conf.close()
+
+        print "Updated backend configuration"
+    else:
+        print ("Backend configuration in %s differs from generated director" %
+               (opts.backend_conf))
+
+    # TODO: Reload Varnish configuration
+
+print "Generated Varnish conf:"
+print director
 
 
-def main():
-    return
-    # generate_homepages()
-    # copy_to_working_dir()
-    # customize_for_servers()
-    # deploy_to_production()
-    # log_generation()
+# def main():
+#     return
+#     # generate_homepages()
+#     # copy_to_working_dir()
+#     # customize_for_servers()
+#     # deploy_to_production()
+#     # log_generation()
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
